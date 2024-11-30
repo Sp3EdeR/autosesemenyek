76a5057c9f685d719b709d378e79a2afe08ef913
Speeder <ek.speeder@gmail.com>
2024-11-30 18:11:54 +0100

    Updated tab selector style to fit the Google Material design

--- calendar.js
+++ calendar.js
@@ -19,51 +19,61 @@ const pageStorage = new class {
 class CalendarTabs {
     constructor(container) {
         this._container = container;
-        this._tabs = container.find('div');
-        this.each(tab => {
+        this._button = container.find('button').first();
+        this._tabs = container.find('.dropdown-item');
+        this._each(tab => {
             const id = tab.attr('id');
             tab.click(() => this.select(id));
         });
 
         var selectedId = 'tab-controller-AGENDA';
-        if (pageStorage.getItem('tabId') && container.find('div#' + selectedId).length)
+        if (pageStorage.getItem('tabId') && container.find('#' + selectedId).length)
             selectedId = pageStorage.getItem('tabId');
         this.select(selectedId);
-    }
-    each(callback) {
-        this._tabs.each(function () { callback($(this)); });
-        return this;
     }
     select(id) {
         if (this.selectedId == id)
             return;
-        this.each(tab => {
-            if (tab.attr('id') == id)
-                tab.addClass('selected');
-            else
-                tab.removeClass('selected');
-        });
         this.selectedId = id;
         pageStorage.setItem('tabId', id);
 
+        const shortIdToName = {
+            'AGENDA': 'Ütemezés',
+            'WEEK': 'Hét',
+            'MONTH': 'Hónap'
+        };
+        var shortId = id.replace('tab-controller-', '');
+        this._button.text(shortIdToName[shortId]);
+
         if (this._onTabChanged)
-            this._onTabChanged(id.replace('tab-controller-', ''));
+            this._onTabChanged(shortId);
     }
     onTabChanged(onTabChanged) { this._onTabChanged = onTabChanged; }
     get currentMode() { return this.selectedId.replace('tab-controller-', ''); }
+    _each(callback) {
+        this._tabs.each(function () { callback($(this)); });
+        return this;
+    }
 }
 
 const calendars = new class Calendars {
     constructor() {
         this._container = $('#calendar-script').parent();
-        this._container.append($(
-            '<iframe style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>' +
-            '<table cellpadding="0" cellspacing="0" class="controls-calendar-tabs"><tr>' +
-            '<td><div id="tab-controller-WEEK">Hét</div></td>' +
-            '<td><div id="tab-controller-MONTH">Hónap</div></td>' +
-            '<td><div id="tab-controller-AGENDA">Napló</div></td></tr></table>' +
-            '<p class="controls-calendar-switches"></p>'
-        ));
+        this._container.append($(`
+<iframe style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+<div class="controls-calendar-view">
+    <button class="dropdown-toggle" type="button" id="viewButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Ütemezés
+    </button>
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="viewButton">
+        <a class="dropdown-item" href="#" id="tab-controller-AGENDA">Ütemezés</a>
+        <a class="dropdown-item" href="#" id="tab-controller-WEEK">Hét</a>
+        <a class="dropdown-item" href="#" id="tab-controller-MONTH">Hónap</a>
+    </div>
+</div>
+<div class="calendar-logo-hider"></div>
+<p class="controls-calendar-switches"></p>
+`));
 
         this._calendarContainer = this._container.children('iframe').first();
         this._switchesContainer = this._container.children('.controls-calendar-switches').first();
@@ -72,7 +82,7 @@ const calendars = new class Calendars {
             '&ctz=Europe%2FBudapest&showTz=0&showPrint=0&showDate=1&showTabs=0&showCalendars=0' +
             '&showTitle=0';
 
-        this.tabs = new CalendarTabs(this._container.children('.controls-calendar-tabs').first());
+        this.tabs = new CalendarTabs(this._container.children('.controls-calendar-view').first());
         this.tabs.onTabChanged(() => this.update());
     }
     init(data) {
